b32a89ae4af9a029a336a70a5924f069983f3b09
DanielGuir <rongze.gui@vanderbilt.edu>
2021-04-14 22:41:27 +0800

    Updated change

---
+++ examples/HelloWorld/driver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 20:21:58 2020
+
+@author: danielgui
+"""
+from transsim.Simulation import Simulation
+
+sim = Simulation(data_path = '.')
+sim.run('helloworld.transsim')
+
+
