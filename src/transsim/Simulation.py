dd9a372a90650dedfde607914d7b7abbc258e82b
abhishek <8484451+adubey14@users.noreply.github.com>
2022-02-19 21:14:49 -0600

    Update Simulation.py

--- src/transsim/Simulation.py
+++ src/transsim/Simulation.py
@@ -3,12 +3,12 @@
 """
 Created on Mon Jan 18 18:18:22 2021
 
-@author: danielgui
+@author: danielgui, abhishek dubey
 """
 from transsim.Interpreter import Interpreter
 import subprocess
 from transsim.Output_Processor import Output_Processor
-import os
+import os,time
 
 class Simulation:
     def __init__(self, data_path, export_path = "./", metamodel_file=None, sumo_home=None, add_path=None):
@@ -26,16 +26,16 @@ class Simulation:
     def run(self, file_name):
         if not file_name.endswith('.transsim'):
             raise ValueError('Wrong extension of program')
-        print('Generating Configuration files for Simulation from ',file_name)
+        print(time.ctime(),":",'Generating Configuration files for Simulation from ',file_name)
         result = self.interpreter.interpret(file_name)
         #result = ['../SUMO_Simulation/Simulation_3/']
-        print('Starting Simulation. Calling Sumo: ','sumo ' + res + 'config.sumocfg')
+        print(time.ctime(),":",'Starting Simulation. Calling Sumo: ','sumo ' + res + 'config.sumocfg')
         for res in result:
-            subprocess.call('sumo ' + res + 'config.sumocfg', shell=True)
-        print('Simulation Complete - Proceeding to output processing')
+            subprocess.call(time.ctime(),":",'sumo ' + res + 'config.sumocfg', shell=True)
+        print(time.ctime(),":",'Simulation Complete - Proceeding to output processing')
         processor = Output_Processor()
         for res in result:
             processor.generate(res)
-        print("All Done")
+        print(time.ctime(),":","All Done")
         
         
